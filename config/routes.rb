b3508b0e3ef7bfa8247049f1d35eb4faad91a285
Kevin Trinh <kxt.trinh@gmail.com>
2022-08-18 15:35:44 +0200

    good sans front end

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
+  # get 'lists/:list_id/bookmarks/new', to: 'bookmarks#new'
+  # get 'lists/:list_id/bookmarks', to: 'bookmarks#create'
+  # get 'bookmarks/:id', to: 'bookmarks#destroy'
+
+  # get 'lists', to: 'lists#index'
+  # get 'lists/:id', to: 'lists#show', as: :list
+  # get 'lists/new', to: 'lists#new'
+  # get 'lists', to: 'lists#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
